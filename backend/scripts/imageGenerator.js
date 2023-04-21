8238d577a7b2598ac574a817bcdc34a08255baa3
EMMANUEL ODOI ODOTEI <93946851+emmanuel-odoi@users.noreply.github.com>
2023-04-21 10:26:38 +0000

    imageGenerator created

---
+++ backend/scripts/imageGenerator.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+export const generateImage = async(prompt) => {
+    return await axios.post('https://api.openai.com/v1/images/generations', JSON.stringify({
+      'prompt': `${prompt}`,
+      'n': 1,
+      'size': '256x256'
+  }), {
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+      }
+  }).then((response) => {
+      return response.data.data[0].url;
+  })
+    .catch((error) => {
+          console.log(`Image generator failed with the error: ${error}`)
+          return '';
+      });
+};
