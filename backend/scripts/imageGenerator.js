654a771fb46815d4d4bf3bd3d35acf87d782ae7d
EMMANUEL ODOI ODOTEI <93946851+emmanuel-odoi@users.noreply.github.com>
2023-04-21 11:44:25 +0000

    modified

--- backend/scripts/imageGenerator.js
+++ backend/scripts/imageGenerator.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 
 export const generateImage = async(prompt) => {
-    return await axios.post('https://api.openai.com/v1/images/generations',{
+    return await axios.post('https://api.openai.com/v1/images/generations', JSON.stringify({
       'prompt': `${prompt}`,
       'n': 1,
       'size': '256x256'
-  }, {
+  }), {
       headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
